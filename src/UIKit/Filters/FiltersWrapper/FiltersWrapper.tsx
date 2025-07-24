233d31128b8895e1dbbdfde01a42a56896377d00
Виктория <imviiktoria@MacBook-Air-Viktoria.local>
2025-07-24 10:46:56 +0400

    feat:<доработка  филтьтр не выбран>

--- src/UIKit/Filters/FiltersWrapper/FiltersWrapper.tsx
+++ src/UIKit/Filters/FiltersWrapper/FiltersWrapper.tsx
@@ -1,41 +1,86 @@
-import React, { PropsWithChildren, useEffect } from 'react'
-import Button from '../../Button/Button'
-import { ButtonType } from '../../Button/ButtonTypes'
+import React, { PropsWithChildren, useEffect, useRef, useState } from "react";
+import Button from "../../Button/Button";
+import { ButtonType } from "../../Button/ButtonTypes";
+import {
+  selectRequestContext,
+  SelectRequestFilters,
+} from "../../../App/stores/SelectRequestContext";
+import {
+  AppFilter,
+  DateFilter,
+  IFilter,
+  IFiltersData,
+  ListFilter,
+  StringFilter,
+} from "../FiltersTypes";
+import { checkHasFilters } from "../../shared/utils/utils";
 
 interface FiltersWrapperProps {
-	resetHandler?: () => void
-	searchHandler?: () => Promise<void>
-	isSearchButtonDisabled?: boolean
+  resetHandler?: () => void;
+  searchHandler?: () => Promise<void>;
+  isSearchButtonDisabled?: boolean;
 }
 
 /** Обертка панели фильтров */
-export default function FiltersWrapper({ searchHandler, resetHandler, children, isSearchButtonDisabled }: PropsWithChildren<FiltersWrapperProps>) {
-	// Обработчик нажатия на enter
-	useEffect(() => {
-		const handleKeyDown = (event: KeyboardEvent) => {
-			if (event.key === "Enter" && searchHandler) searchHandler()
-			removeListener()
-		};
-		
-		const removeListener = () => {
-			document.removeEventListener("keydown", handleKeyDown)
-		}
+export default function FiltersWrapper({
+  searchHandler,
+  resetHandler,
+  children,
+  isSearchButtonDisabled,
+}: PropsWithChildren<FiltersWrapperProps>) {
+  const { data, setValue } = selectRequestContext.useContext();
 
-		document.addEventListener("keydown", handleKeyDown);
-		
-		return () => removeListener()
-	}, [searchHandler]);
+  // Обработчик нажатия на enter
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Enter") searchHandlerWrapper();
+      removeListener();
+    };
 
-	return (
-		<div className="filters-wrapper">
-			<div className="filters-wrapper__header">фильтр</div>
-			<div className="filters-wrapper__buttons">
-				<Button title={"сбросить"} buttonType={ButtonType.outline} clickHandler={resetHandler} />
-				<Button title={"поиск"} clickHandler={searchHandler} disabled={isSearchButtonDisabled} />
-			</div>
-			<div className="filters-wrapper__list">
-				{children}
-			</div>
-		</div>
-	)
+    const removeListener = () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => removeListener();
+  }, [searchHandler]);
+
+  // Показывать ошибку "Фильтр не выбран"?
+  const [isShowEmptyFiltersError, setIsShowEmptyFiltersError] =
+    useState<boolean>(false);
+
+  // Обертка для функции поиска с проверкой выбранных фильтров
+  const searchHandlerWrapper = () => {
+    const hasFilters = checkHasFilters(data.filters);
+    setIsShowEmptyFiltersError(!hasFilters);
+
+    if (searchHandler && hasFilters) searchHandler();
+  };
+
+  return (
+    <div className="filters-wrapper">
+      <div className="filters-wrapper__header">фильтр</div>
+      <div className="filters-wrapper__buttons">
+        <Button
+          title={"сбросить"}
+          buttonType={ButtonType.outline}
+          clickHandler={resetHandler}
+        />
+        <div className="filters-wrapper__buttons_search-button">
+          <Button
+            title={"поиск"}
+            clickHandler={searchHandlerWrapper}
+            disabled={isSearchButtonDisabled}
+          />
+          {isShowEmptyFiltersError && (
+            <div className="filters-wrapper__error-message">
+              Фильтр не выбран
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="filters-wrapper__list">{children}</div>
+    </div>
+  );
 }
--- src/UIKit/shared/utils/utils.ts
+++ src/UIKit/shared/utils/utils.ts
@@ -1,29 +1,55 @@
+import {
+  AppFilter,
+  DateFilter,
+  IFiltersData,
+  ListFilter,
+  StringFilter,
+} from "../../Filters/FiltersTypes";
+
 /** Маршрутизация по SPA */
 export const redirectSPA = (href: string) => {
-	let element = document.createElement('a')
-	element.href = href
-	element.style.display = 'none'
-	document.querySelector('body')?.appendChild(element)
-	element.click()
-	element.remove()
-}
+  let element = document.createElement("a");
+  element.href = href;
+  element.style.display = "none";
+  document.querySelector("body")?.appendChild(element);
+  element.click();
+  element.remove();
+};
 
 /** Установка debounce */
-export const setDebounce = (callback: (...args: any) => any, delay: number = 100) => {
-	console.log('test')
-	/** Идентификатор отложенного вызова */
-	let timeoutId: number | undefined = undefined
+export const setDebounce = (
+  callback: (...args: any) => any,
+  delay: number = 100
+) => {
+  console.log("test");
+  /** Идентификатор отложенного вызова */
+  let timeoutId: number | undefined = undefined;
 
-	const call = () => {
-		console.log(timeoutId)
-		if (timeoutId) clearTimeout(timeoutId)
-		timeoutId = setTimeout(callback, delay)
-	}
+  const call = () => {
+    console.log(timeoutId);
+    if (timeoutId) clearTimeout(timeoutId);
+    timeoutId = setTimeout(callback, delay);
+  };
 
-	return call
+  return call;
+};
+
+/** Проверка указаны ли фильтры */
+export function checkHasFilters(filters: IFiltersData): boolean {
+  for (const key in filters) {
+    const filter = filters[key];
+
+    if ((filter as AppFilter).value?.code) return true;
+    if ((filter as StringFilter).value) return true;
+    if ((filter as ListFilter).values?.length) return true;
+    if ((filter as DateFilter).valueFrom || (filter as DateFilter).valueTo)
+      return true;
+  }
+
+  return false;
 }
 
 export default {
-	redirectSPA,
-	setDebounce,
-}
+  redirectSPA,
+  setDebounce,
+};
